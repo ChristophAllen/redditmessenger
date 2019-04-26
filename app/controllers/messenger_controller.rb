59eee591d87b0a542dcc8e6dbac226695e03651f
Christopher Allen <chriscma@gmail.com>
2019-04-25 17:51:05 -1000

    working state

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -91,26 +91,33 @@ class MessengerController < ApplicationController
 		pass = driver.find_element(name: 'password')
 		pass.send_keys "Guppy303*"
 		pass.submit
-		sleep 5
+		sleep 10
 		driver.navigate.to "https://www.reddit.com/message/compose/"
-		sleep 5
+		sleep 10
 		frame = driver.find_elements(:tag_name, "iframe")
-		sleep 5
+		sleep 10
 		driver.switch_to.frame(0)
-		sleep 5
+		sleep 10
 		to = driver.find_element(name: 'to')
-		text = driver.find_element(name: 'text')
+		subject = driver.find_element(name: 'subject')
+		text = driver.find_elements(name: 'text')
 		submit = driver.find_element(name: 'send')
-		Nurse.all.each do |x|
-			sleep 5
-			if x.messagesent == true
+		# Nurse.all.each do |x|      
+			sleep 3
+			to.send_keys x.name   #doesn't belong 
+			if x.messagesent == true || to.attribute('value')
 
 			else
-				to.send_keys x.name
-				subject.send_keys subjecttemplate()
-				text.send_keys texttemplate()
+				# to.send_keys x.name
+				# sleep 2
+				# subject.send_keys subjecttemplate()
+				# sleep 2
+				# text[1].send_keys texttemplate()
+				# sleep 8
+				# x.update_column(:messagesent, true)
+				# submit.click
 			end
-		end
+		# end
 	end
 
 
@@ -121,17 +128,7 @@ class MessengerController < ApplicationController
 	end
 
 	def texttemplate
-		return "Hey, sorry if this is a bother but I found your username through the nursing subreddit.  
-		I’m an amateur web developer who is trying to build a resource to help young people decide what 
-		they want to do in life.  Just want to make something cool while I’m saving up to go back to school.
-		  The idea I’ve built is a video upload platform where young people can go to watch others talk about 
-		  what a day is like in their life in regards to there job.  To maybe help them decide what they want 
-		  to do in life.  Calling it DayInTheLifeOf.  And I’m on the part where I’m trying to get videos.  
-		  	Would you be interested in taking a few minute video of yourself talking about what it’s like to 
-		  	be a nurse(**don’t have to show your face**)?  And I bought some meager gold to give out for anyone 
-		  	interested.  The site is going to be free to use, and ad free if that matters to you.  If you are interested 
-		  	I have a short list of questions I could send to give an idea of things to talk about, and a dropbox link to
-		  	 upload the video to.  If not, no worries and my apologies for bothering."
+		return "Hey, sorry if this is a bother but I found your username through the nursing subreddit.  I’m an amateur web developer who is trying to build a resource to help young people decide what they want to do in life.  Just want to make something cool while I’m saving up to go back to school.  The idea I’ve built is a video upload platform where young people can go to watch others talk about what a day is like in their life in regards to there job.  To maybe help them decide what they want to do in life.  Calling it DayInTheLifeOf.  And I’m on the part where I’m trying to get videos.  Would you be interested in taking a few minute video of yourself talking about what it’s like to be a nurse(**don’t have to show your face**)?  And I bought some meager gold to give out for anyone interested.  The site is going to be free to use, and ad free if that matters to you.  If you are interested I have a short list of questions I could send to give an idea of things to talk about, and a dropbox link to upload the video to.  If not, no worries and my apologies for bothering."
 	end
 
 end
